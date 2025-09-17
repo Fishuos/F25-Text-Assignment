e00d4d8b29b2aa9462970258ff546c63c6a60009
Gage Parsons <gageparsons@outlook.com>
2025-09-17 17:56:04 -0400

    added journal

--- Ghost-Text-Assignment/Program.cs
+++ Ghost-Text-Assignment/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Narrator: Boo! Youre a ghost in a haunted mansion!");
-Console.WriteLine("Narrator: Luckily, youre the one haunting it");
+Console.WriteLine("Narrator: Luckily, you're the one haunting it.");
 Console.WriteLine("");
 Console.WriteLine("Narrator: Its not a mansion if you dont have your favourite room!");
 Console.WriteLine("Narrator: By the way, what is your favourite room in this mansion you call home?");
@@ -10,9 +10,9 @@ Console.WriteLine("");
 Console.WriteLine($"Narrator: Wow! The {favouriteRoom} is my favourite room too!");
 Console.WriteLine("");
 Console.WriteLine("");
-Console.WriteLine("Narrator: You hear something outside... it sounds like a vehicle");
+Console.WriteLine("Narrator: You hear something outside... it sounds like a vehicle.");
 Console.WriteLine("Narrator: In through the door bursts 4 ghost hunters!");
-Console.WriteLine("Narrator: One of them finds the ouiji board you left out, he asks");
+Console.WriteLine("Narrator: One of them finds the ouiji board you left out, he asks...");
 Console.WriteLine("Hunter: What year did you die?");
 Console.WriteLine("");
 
@@ -25,8 +25,8 @@ Console.WriteLine($"Hunter: Wow this ghost died {died} years ago!");
 Console.WriteLine("");
 
 
-Console.WriteLine("Narrator: You see the hutners scramble around a bit more, before one pulls out a small radio like device");
-Console.WriteLine("Narrator: One of the hunters speak into the radio and say");
+Console.WriteLine("Narrator: You see the hunters scramble around a bit more, before one pulls out a small radio like device.");
+Console.WriteLine("Narrator: One of the hunters speak into the radio and say...");
 Console.WriteLine("Hunter: Do you wish to hurt us.");
 
 string hurt = Console.ReadLine();
@@ -48,9 +48,9 @@ else
 }
 
 Console.WriteLine("");
-Console.WriteLine("Narrator: You hear a flick come from the basement, followed by");
+Console.WriteLine("Narrator: You hear a flick come from the basement, followed by...");
 Console.WriteLine("Hunter: Generators on!");
-Console.WriteLine("Narrator: You see one of the hunters flick a light switch on, and the blaring lights come to life");
+Console.WriteLine("Narrator: You see one of the hunters flick a light switch on, and the blaring lights come to life.");
 Console.WriteLine("Narrator: do you wish to leave the light on, or turn it back off?");
 Console.WriteLine("");
 
@@ -61,25 +61,79 @@ string findOff = "off";
 //looks for the word "on", incase user types "turn on" instead of just "on"
 if (lights.Contains(findOn))
 {
-    Console.WriteLine("Narrator: You leave the lights on, but start to get this strange feeling");
-    Console.WriteLine("Narrator: Your head starts pounding, and you start feeling dizzy");
-    Console.WriteLine("Narrator: You snap out of it, and as you do the light bulbs in the room explode, leaving you in darkness");
+    Console.WriteLine("Narrator: You leave the lights on, but start to get this strange feeling.");
+    Console.WriteLine("Narrator: Your head starts pounding, and you start feeling dizzy.");
+    Console.WriteLine("Narrator: You snap out of it, and as you do the light bulbs in the room explode, leaving you in darkness.");
 }
 //looks for the word "off", incase user types "turn off" instead of just "off"
 else if (lights.Contains(findOff))
 {
-    Console.WriteLine("Narrator: you rush to the lights as fast as you can and switch them back off");
+    Console.WriteLine("Narrator: you rush to the lights as fast as you can and switch them back off.");
     Console.WriteLine("Narrator: The hunters flinch at this suddon action.");
 }
 
 else
 {
-    Console.WriteLine($"Narrator: {lights} is invalid, you left the lights on");
-    Console.WriteLine("Narrator: You leave the lights on, but start to get this strange feeling");
-    Console.WriteLine("Narrator: Your head starts pounding, and you start feeling dizzy");
-    Console.WriteLine("Narrator: You snap out of it, and as you do the light bulbs in the room explode, leaving you in darkness");
+    Console.WriteLine($"Narrator: {lights} is invalid");
+    Console.WriteLine("Narrator: You leave the lights on, but start to get this strange feeling.");
+    Console.WriteLine("Narrator: Your head starts pounding, and you start feeling dizzy.");
+    Console.WriteLine("Narrator: You snap out of it, and as you do the light bulbs in the room explode, leaving you in darkness.        ");
 }
 
 
 
 Console.WriteLine("");
+Console.WriteLine("Hunter: Lets leave this room, I dont think the ghost wants us here.");
+Console.WriteLine($"Narrator: You see the hunters enter the {favouriteRoom}");
+Console.WriteLine("Narrarator: you see one of the hunters pull out a video recorder, and start pointing it around the room.");
+Console.WriteLine("Hunter: I see ghost orbs! This must be its room! Go grab the book and see if they write their name in it.");
+Console.WriteLine("Narrator: You see one of the hunters leave for a moment before coming back and placing a book on the floor.");
+Console.WriteLine("Narrator: What is your name?");
+Console.WriteLine("");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hunter: Wait... this is THE {name}... We gotta get out of here!");
+Console.WriteLine("Narrator: you see all of the hunters scramble out of the building as fast as they can, leaving behind equiptment and a single journal.");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+
+
+// Repeats the overall gathered info 
+Console.WriteLine("Ghost Hunting Journal.");
+Console.WriteLine("");
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Time since death: {died} years");
+Console.WriteLine($"Favourite room: {favouriteRoom}");
+
+
+if (hurt.ToLower() == "yes")
+{
+    Console.WriteLine("Mood: Agressive");
+}
+
+else if (hurt.ToLower() == "no")
+{
+    Console.WriteLine("Mood: Friendly");
+}
+
+else
+{
+    Console.WriteLine("Mood: Unknown...");
+}
+
+if (lights.Contains(findOn))
+{
+    Console.WriteLine("Extra notes: Blew up a lightbulb");
+ 
+}
+else if (lights.Contains(findOff))
+{
+    Console.WriteLine("Extra notes: flicked light switch off as soon as it was turned on");
+  
+}
+
+Console.WriteLine("Evidence's found: Spirit box, Ghost Writing, Ghost Orbs ");
+Console.WriteLine("Ghost type: The Mare");
