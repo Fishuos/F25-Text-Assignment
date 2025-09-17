8e05dca94c84f69d7bac2ce31ef4cc0f164c9e0e
Gage Parsons <gageparsons@outlook.com>
2025-09-16 20:01:27 -0400

    added a "you died blank years ago" and favourite room

---
+++ Deconstruction/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("what is your name?");
+Console.WriteLine("");
+
+String Name = Console.ReadLine();
+string yourName = Name;
+
+Console.WriteLine("");
+Console.WriteLine("how much gold do you have?");
+Console.WriteLine("");
+
+int Gold = int.Parse(Console.ReadLine());
+int yourGold = Gold;   
+
+Console.WriteLine("a bronze sword costs 15 gold");
+
+if (yourGold < 15)
+{
+    Console.WriteLine($"uh oh! sorry {yourName}, you dont have enough gold!");
+}
+
+else 
+{
+    yourGold -= 15;
+    Console.WriteLine("you buy the bronze sword!");
+    Console.WriteLine($"its a nice sword, {yourName}, you now only have {yourGold} gold left!");
+}
--- Ghost-Text-Assignment/Program.cs
+++ Ghost-Text-Assignment/Program.cs
@@ -1,11 +1,33 @@
-﻿Console.WriteLine("what is your favourite room!");
+﻿Console.WriteLine("Boo! Youre a ghost in a haunted mansion!");
+Console.WriteLine("Luckily, youre the one haunting it");
+Console.WriteLine("");
+Console.WriteLine("Its not a mansion if you dont have your favourite room!");
+Console.WriteLine("by the way, what is your favourite room in this mansion you call home?");
+Console.WriteLine("");
+string room = Console.ReadLine();
 
-Console.WriteLine("Bedroom");
-Console.WriteLine("Bathroom");
-Console.WriteLine("Closet");
+string favouriteRoom = room;
+Console.WriteLine("");
+Console.WriteLine($"Wow! The {favouriteRoom} is my favourite room too!");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("You hear something outside... it sounds like a vehicle");
+Console.WriteLine("in through the door bursts 4 ghost hunters!");
+Console.WriteLine("one of them finds the ouiji board you left out, he asks");
+Console.WriteLine("What year did you die?");
+Console.WriteLine("");
 
-string Input = Console.ReadLine();
+//finds out how many years ago you died
+int death = int.Parse(Console.ReadLine());
+int yourDeath = death;
+int currentYear = DateTime.Now.Year;
+int died = currentYear - yourDeath;
+Console.WriteLine("");
+Console.WriteLine($"Wow this ghost died {died} years ago!");
+Console.WriteLine("");
 
-string favouriteRoom = Input;
 
-Console.WriteLine($"{favouriteRoom}");
+Console.WriteLine("You see the hutners scramble around a bit more, before one pulls out a small radio like device");
+Console.WriteLine("one of the hunters speak into the radio and say");
+Console.WriteLine("Do you want to hurt us.");
+
